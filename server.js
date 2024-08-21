dc23d907f7b08893afc9de5d5b3d83c0c5fbe372
Bao Hoang <ghoa0004@student.monash.edu>
2024-08-22 02:23:21 +1000

    add database connection & example database select query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pool = require('./database');
 const path = require('path');
 
 const app = express();
@@ -6,6 +7,13 @@ const port = process.env.PORT || 3000;
 
 // Serve files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+async function testGet(){
+    const res = await pool.query("select * from public.user;")
+    console.log(res.rows)
+}
+
+testGet()
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
