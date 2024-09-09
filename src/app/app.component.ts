bd41e48cec18d7fd63ce285be1bfda5c79ef06c4
Bao Hoang <ghoa0004@student.monash.edu>
2024-09-09 12:45:07 +1000

    basic nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
