90458d395e77822262a7484e2e0bae7d867fb3d7
jzho0123 <jzho0123@student.monash.edu>
2024-10-07 03:47:05 +1100

    add scroll down button to many pages and add 2 buttons at the bottom of the carreer page

--- public/js/insight.js
+++ public/js/insight.js
@@ -767,3 +767,21 @@ document.addEventListener("DOMContentLoaded", function () {
         createEmployedWomenBarChart(employedYearSelector.value);
     });
 });
+
+// scroll up bottom
+// Get the button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.classList.add("show");
+    } else {
+        scrollToTopBtn.classList.remove("show");
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
